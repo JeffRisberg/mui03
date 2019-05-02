fa114190ead551ee779b447b3e807c5f8c1a77fa
JeffRisberg <jrisberg@aol.com>
2019-05-01 23:39:01 -0700

    add avatar example

--- src/scenes/Catalog.js
+++ src/scenes/Catalog.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 import {withStyles} from '@material-ui/core/styles';
 
 /**
@@ -10,12 +12,22 @@ import {withStyles} from '@material-ui/core/styles';
 const styles = (theme) => ({
    grid: {
       height: '50px',
-   }
+   },
+   avatar: {
+      margin: 10,
+   },
+   bigAvatar: {
+      margin: 10,
+      width: 60,
+      height: 60,
+   },
 });
 
 class Catalog extends Component {
 
    render() {
+      const { classes } = this.props;
+
       return (
          <div>
             <Card>
@@ -23,6 +35,11 @@ class Catalog extends Component {
                   <Typography gutterBottom variant="h4">
                      Catalog
                   </Typography>
+
+                  <Grid container justify="center" alignItems="center">
+                     <Avatar alt="Remy Sharp" src="/static/images/avatars/1.jpg" className={classes.avatar} />
+                     <Avatar alt="Remy Sharp" src="/static/images/avatars/1.jpg" className={classes.bigAvatar} />
+                  </Grid>
                </CardContent>
             </Card>
          </div>
